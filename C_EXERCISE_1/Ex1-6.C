94564b4d3d9ee8716badd8ab289009416aaa916b
VedC <vedvarunc@gmail.com>
2024-12-05 23:09:51 -0500

    almostdone

--- C_EXERCISE_1/Ex1-6.C
+++ C_EXERCISE_1/Ex1-6.C
@@ -7,4 +7,21 @@ int main () {
     blank=0;
     tab=0;
 
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {       // Check if the character is a blank (space)
+            blank++;
+        } else if (c == '\t') { // Check if the character is a tab
+            tabs++;
+        } else if (c == '\n') { // Check if the character is a newline
+            newlines++;
+        }
+    }
+
+    // Print the counts of blanks, tabs, and newlines
+    printf("Blanks: %d\n", blank);
+    printf("Tabs: %d\n", tabs);
+    printf("Newlines: %d\n", newln);
+
+    return 0; // Exit the program successfully
+}
 
