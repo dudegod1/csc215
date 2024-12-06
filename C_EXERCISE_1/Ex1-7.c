7d14a7535e80f41bf808651a2422d9d5a489a7be
VedC <vedvarunc@gmail.com>
2024-12-05 23:13:45 -0500

    initializedvars

---
+++ C_EXERCISE_1/Ex1-7.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+main() /* Replace multiple blanks with a single blank */
+{
+    int c, prev_char; // c: current character, prev_char: previous character
+    prev_char = 0;    // Initialize previous character to 0
+
