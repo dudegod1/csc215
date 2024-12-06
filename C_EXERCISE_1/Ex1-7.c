9215eaa21ccf7ea9f6cfc2d0df684ff6c504f126
VedC <vedvarunc@gmail.com>
2024-12-05 23:15:47 -0500

    finaleditsdone

--- C_EXERCISE_1/Ex1-7.c
+++ C_EXERCISE_1/Ex1-7.c
@@ -4,4 +4,12 @@ main() /* Replace multiple blanks with a single blank */
 {
     int c, prev_char; // c: current character, prev_char: previous character
     prev_char = 0;    // Initialize previous character to 0
+    
+    while ((c = getchar()) != EOF) {
+        if (c != ' ' || prev_char != ' ') {
+            putchar(c);
+        }
+        prev_char = c; //update previous character
 
+    }
+}
