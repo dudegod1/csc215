4c506e335807355364c959c763e89947767ee0d6
VedC <vedvarunc@gmail.com>
2024-12-09 22:59:56 -0500

    mainfunc

--- chrnums.C
+++ chrnums.C
@@ -9,3 +9,19 @@ main(argc, argv)
     int c, len;
     c = len = 0;
     char words[MAXLINE];
+
+     if (argc != 2){
+        printf("Usage: CHRNUMS <infile>\n");
+        exit();
+    }
+    if((infp = fopen(argv[1], "r")) == NULL){
+        printf("Can't open %s\n", argv[1]);
+        exit();
+    }else{
+        infp = fopen(argv[1], "r")
+        while ((c = fgetc(infp)) != EOF) {
+            if (c == "\n"){
+                words[len] = '\0'; /* If it's a new line, it will make that position to nUll*/
+                printf("%d: %s\n", len, words);
+                len = 0;
+                continue;
