a61d5adee74e88f4054cb05034efbaa70135b285
VedC <vedvarunc@gmail.com>
2025-01-19 09:01:39 -0500

    madeadditions

--- casestudy/bigInt.c
+++ casestudy/bigInt.c
@@ -1,4 +1,19 @@
 #include "BIGINT.H"
-#include
 
+void set_bigint(numstr, num)
+char *numstr;
+struct bigint *num;
+{
+    char last_pos, i;
+    num->negative = (numstr[0] == '-');
+    num->numdigits = strlen(numstr) - num->negative;
+    num->digits = alloc(num->numdigits);
+    last_pos = num->numdigits + (num->negative ? 0 : -1);
 
+    for (i = 0; i < num->numdigits; i++) {
+        num->digits[i] = numstr[last_pos-i];
+        /* printf("numstr[%d] is %c\n", last_pos-i, numstr[last_pos-i]); */
+    }
+}
+
+`
