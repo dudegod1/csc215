189f2373f27d4929f2a746a838f328c83ae5a5e0
VedC <vedvarunc@gmail.com>
2025-01-19 09:04:45 -0500

    finished

--- casestudy/bigInt.c
+++ casestudy/bigInt.c
@@ -16,4 +16,18 @@ struct bigint *num;
     }
 }
 
-`
+char* get_bigint(num)
+struct bigint *num;
+{
+   char *numstr;
+   char start_pos, i;
+   numstr = alloc(num->numdigits + (num->negative ? 2 : 1));
+   start_pos = num->negative;
+   if (start_pos == 1) numstr[0] = '-';
+   for (i = 0; i < num->numdigits; i++) {
+       numstr[i+start_pos] = num->digits[num->numdigits-(i+1)];
+       /* printf("numstr[%d] is %c\n", i, numstr[i+start_pos]); */
+   }
+   numstr[num->numdigits+start_pos] = '\0';
+   return numstr;
+}
