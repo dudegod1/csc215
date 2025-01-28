af0f5ae993b0e831f53b5efc02f42a53c2bf0809
VedC <vedvarunc@gmail.com>
2025-01-28 09:15:31 -0500

    bigint.hfixed

--- bigint2/src/bigint.h
+++ bigint2/src/bigint.h
@@ -14,4 +14,20 @@ class BigInt
 
     // member functions
     string to_string() const;
+    
+    //comparisons 
+    bool operator==(const BigInt&) const;
+    bool operator>(const BigInt&) const;
+    bool operator!=(const BigInt&) const;
+    bool operator<=(const BigInt&) const;
+    bool operator<(const BigInt&) const;
+    bool operator>=(const BigInt&) const;
+    
+
+    //operations
+    BigInt operator+(const BigInt& b2) const;
+    BigInt operator-() const;
+    BigInt operator-(const BigInt &b2) const ;
+    BigInt operator*(const BigInt &b2) const;
+
 };
